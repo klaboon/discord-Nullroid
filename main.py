643be49928f67dd76e8a0d23ddd3862d8451405a
klaboon <p.lai.05pers@gmail.com>
2024-08-29 18:12:07 -0700

    basic prototype

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-#from discord import Intents, Cilent, Message
-#from responses import get_response
+from discord import Intents, Client, Message
+from responses import get_response
 
 # step 0: load token from somewhere safe
 load_dotenv()
@@ -10,9 +10,9 @@ print(TOKEN)
 
 # step 1: bot setup
 
-intents = intents.default()
+intents = Intents.default()
 intents.message_content = True #NOQA
-cilent = Cilent(intents=intents)
+client = Client(intents=intents)
 
 # step 2: message functionality
 async def send_message(message: Message, user_message: str) -> None:
@@ -20,12 +20,38 @@ async def send_message(message: Message, user_message: str) -> None:
         print('(Message was empty because intents were not enabled possibly)')
         return
 
-        if is_private := user_message[0] == '?':
-            user_message = user_message[1:]
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
 
-        try:
-            response: str = get_response(user_message)
-            await message.author.send(response) if is_private else message.channel.send(response)
-        except Exception as e: # practice logging instead of printing
-            print(e)
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e: # practice logging instead of printing
+        print(e)
 
+# step 3: handling startup for bot
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+# step 4: handling incoming messages
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+
+# setp 5: main entry point
+def main() -> None: 
+    client.run(token=TOKEN)
+
+
+if __name__ == '__main__':
+    main()
--- responses.py
+++ responses.py
@@ -1,7 +1,7 @@
 from random import choice, randint
 
 
-def get_response(user_input: str) -> str:
+def get_response(user_input: str) -> str: 
     lowered: str = user_input.lower()
     
     if lowered == '':
